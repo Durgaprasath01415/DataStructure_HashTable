4427a21d5ca13e94b4e297dd9b9598ad67babd4d
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-16 10:22:44 +0530

    ADDED : Remove word from Sentence

--- src/test/java/MyHashMapParanoidTest.java
+++ src/test/java/MyHashMapParanoidTest.java
@@ -3,22 +3,40 @@ import org.junit.Test;
 
 public class MyHashMapParanoidTest {
         @Test
-        public void givenALargeSentenceWhenWordsAreAddedToListShouldReturnParanoidWordFrequency() {
+        public void givenALargeSentence_WhenWordsAreAddedToList_ShouldReturnParanoidWordFrequency() {
             String sentence = "Paranoids are not paranoid because they are paranoid but"
                     + " because they keep putting themselves deliberately into paranoid "
                     + "avoidable situations";
-            MyHashMapParanoid<String, Integer> myHashMap = new MyHashMapParanoid<>();
+            MyHashMapParanoid<String, Integer> myHashMapParanoid = new MyHashMapParanoid<>();
             String[] words = sentence.toLowerCase().split(" ");
             for (String word : words) {
-                Integer value = myHashMap.get(word);
+                Integer value = myHashMapParanoid.get(word);
                 if (value == null)
                     value = 1;
                 else
                     value += 1;
-                myHashMap.add(word, value);
+                myHashMapParanoid.add(word, value);
             }
-            int frequency = myHashMap.get("paranoid");
-            System.out.println(myHashMap);
+            int frequency = myHashMapParanoid.get("paranoid");
+            System.out.println(myHashMapParanoid);
             Assert.assertEquals(3, frequency);
         }
+
+    @Test
+    public void givenASentence_WhenWordIsRemoved_ShouldReturnDeletedWordFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but because "
+                + "they keep putting themselves deliberately into paranoid avoidable situations";
+        MyHashMapParanoid<String, Integer> myHashMapParanoid = new MyHashMapParanoid<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myHashMapParanoid.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value += 1;
+            myHashMapParanoid.add(word, value);
+        }
+        int frequency = myHashMapParanoid.remove("avoidable");
+        Assert.assertEquals(1, frequency);
+    }
 }
